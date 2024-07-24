61e17570593af28f1c9652433d07c05c3824f213
chenjujun <0431cjj@163.com>
2024-07-24 14:40:18 +0800

    测试插件

--- spring-hot-plugin-example/plugin-demo/src/main/java/csdn/itsaysay/plugin/IndexController.java
+++ spring-hot-plugin-example/plugin-demo/src/main/java/csdn/itsaysay/plugin/IndexController.java
@@ -1,13 +1,28 @@
 package csdn.itsaysay.plugin;
 
+import com.alibaba.fastjson.JSON;
+import csdn.itsaysay.plugin.bean.Person;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(){
+    @GetMapping({"/hello"})
+    public String hello() {
         return "Hello itsaysay!";
+    }
+
+    @GetMapping({"/hello/name"})
+    public Person helloName() {
+        Person person = new Person();
+        person.setName("Human");
+        String json = JSON.toJSONString(person);
+        log.info(json);
+        return person;
     }
 }
---
+++ spring-hot-plugin-example/plugin-demo/src/main/java/csdn/itsaysay/plugin/bean/Person.java
@@ -0,0 +1,10 @@
+package csdn.itsaysay.plugin.bean;
+
+
+import lombok.Data;
+
+@Data
+public class Person {
+
+    private String name;
+}
