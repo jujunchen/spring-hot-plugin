ca683ca2ced6012715c015ef65ed02ad92191412
chenjujun <0431cjj@163.com>
2024-07-31 02:20:51 +0800

    第三方dll 文件对接 demo

--- spring-hot-plugin-example/plugin-demo/src/main/java/csdn/itsaysay/plugin/IndexController.java
+++ spring-hot-plugin-example/plugin-demo/src/main/java/csdn/itsaysay/plugin/IndexController.java
@@ -1,20 +1,16 @@
 package csdn.itsaysay.plugin;
 
 import com.alibaba.fastjson.JSON;
-import csdn.itsaysay.demo.plugin.service.PluginService;
+import com.sun.jna.Native;
 import csdn.itsaysay.plugin.bean.Person;
+import csdn.itsaysay.plugin.hik.HCNetSDK;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-
 @Slf4j
 @RestController
 public class IndexController {
-
-    @Resource
-    private PluginService pluginService;
 
     @GetMapping({"/hello"})
     public String hello() {
@@ -28,5 +24,17 @@ public class IndexController {
         String json = JSON.toJSONString(person);
         log.info(json);
         return person;
+    }
+
+    @GetMapping("/hik/sdk/init")
+    public Boolean hikSDKInit() {
+        //海康的SDK dll依赖了其他的dll，需要指定目录，放插件包没用
+        //只有一个dll的sdk，不需要，可以放在插件包内
+        //这是java native的限制
+        HCNetSDK hCNetSDK = (HCNetSDK) Native.loadLibrary("D:\\javaprojects\\spring-hot-plugin\\spring-hot-plugin-example\\plugin-demo\\target\\classes\\win32-x86-64\\HCNetSDK.dll", HCNetSDK.class);
+        if (hCNetSDK == null) {
+            return false;
+        }
+        return true;
     }
 }
