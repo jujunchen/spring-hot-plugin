fd844efdd82c417a8537911dcbb8d9a5240faa95
chenjujun <0431cjj@163.com>
2024-08-02 01:06:13 +0800

    新增mybatis组件

---
+++ spring-hot-plugin-mybatis/src/main/java/csdn/itsaysay/plugin/mybatis/MybatisConfiguration.java
@@ -0,0 +1,17 @@
+package csdn.itsaysay.plugin.mybatis;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisConfiguration {
+
+    @Bean
+    @ConditionalOnClass(SqlSession.class)
+    public RegisterMybatis createRegisterMybatis(ApplicationContext main) {
+        return new RegisterMybatis(main);
+    }
+}
